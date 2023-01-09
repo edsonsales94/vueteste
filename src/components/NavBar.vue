195917c328bfae7d872913bd96fea8857b54ded9
Edson Sales <edson.sales94@outlook.com>
2023-01-09 16:58:19 -0400

    commit inicial: exclusão dos arquivos padrões do vue que não seram utilizados e criacão do componente NavBar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="header">
+        <nav class="container">
+            <a href="/">
+                <h2 class="logo">SYSTEM :)</h2>
+            </a>
+            <div class="menu-item">
+                <router-link class="text-nav" to="/">Produtos</router-link>
+                <router-link class="text-nav" to="/about">Clientes</router-link>
+                <router-link class="text-nav" to="/about">Cli x Prod</router-link>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    data() {
+    }
+}
+</script>
+
+
+<style lang="sass" scoped>
+    div.header
+        padding: 50px 0
+        background: #13141f
+        font: 1.5em sans-serif
+    a
+        text-decoration: none
+        color: #fff
+    .text-nav 
+        color: #fff
+        margin: 10px
+        text-decoration: none
+        color: white
+        font-size: 25px
+        text-decoration: none
+        transition: 0.5s
+    .text-nav:hover 
+        color: rgb(88, 96, 139)
+        font-weight: bold
+        padding: auto   
+    .container
+        width: 100%
+        display: flex
+        align-content: space-between
+        align-items: center
+    
+    @media (max-width: 600px)
+        div.header
+            font: 0.8rem sans-serif
+        .text-nav 
+            font-size: 14px
+            
+</style>

d93e86a9aa7f4e6e86df1e70fb9f566257ad93c0
Edson Sales <edson.sales94@outlook.com>
2023-01-10 16:30:32 -0400

    Alteracão do backgroud do componente ClientPage, trabalhei na correcao do componente ProductPage fiz o os script o methods para enviar os dados para API. e crie um Json-server para usar como api fake e armazenar os dados

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,117 +1,147 @@
-<!-- <template>
-    <div class="container">
-        <div>
-            <label for="productname">Descrição Produto </label>
-            <input type="text">
-        </div>
-        <div>
-            <span>Ativo: </span>
-            <label for="type">Sim</label>
-            <input type="radio">
-            <label for="type">Não</label>
-            <input type="radio">
-        </div>
-
-    </div>
-</template> -->
 <template>
-  <Message :msg="msg" v-show="msg" />
-  <div>
-    <form id="burger-form" method="POST" @submit="createBurger">
-      <div class="input-container">
-        <label for="nome">Nome do cliente:</label>
-        <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
-      </div>
-      <div class="input-container">
-        <label for="pao">Escolha o pão:</label>
-        <select name="pao" id="pao" v-model="pao">
-          <option value="">Selecione o seu pão</option>
-          <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
-        </select>
-      </div>
-      <div class="input-container">
-        <label for="carne">Escolha a carne do seu Burger:</label>
-        <select name="carne" id="carne" v-model="carne">
-          <option value="">Selecione o tipo de carne</option>
-          <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
-        </select>
-      </div>
-      <div id="opcionais-container" class="input-container">
-        <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-        <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
-          <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
-          <span>{{ opcional.tipo }}</span>
+    <form class="container-form">
+        <div class="container-input">
+            <label for="name">Nome Cliente: </label>
+            <input type="text" v-model="name">
         </div>
-      </div>
-      <div class="input-container">
-        <input class="submit-btn" type="submit" value="Criar meu Burger!">
-      </div>
+        <div class="container-input">
+            <div class="radio">
+                <label for="flag" style="margin-right: 15px;">Ativo</label>
+                <input class="radio-btn" type="radio" id="flag" v-model="ativo" :value="true">
+            </div>
+            <div class="radio">
+                <label for="flag">Inativo</label>
+                <input class="radio-btn" type="radio" id="flag" v-model="ativo" :value="false">
+            </div>
+        </div>
+        <div class="container-input">
+            <input  class="submit-btn" type="submit" @click="cadProduto" value="Cadastrar Produto">
+        </div>
+        <div class="container-input">
+            <span class="messege">{{ msg }}</span>
+            <span class="error">{{ error }}</span>
+        </div>
     </form>
-  </div>
 </template>
+
 <script>
 export default {
+    name: "Product",
+    data() {
+        return {
+            name: '',
+            ativo: false,
+            msg: null,
+            error: ''
+        }
+    },
+    methods: {
+        async cadProduto(e) {
+
+            if (this.name!=="") {
+
+                e.preventDefault()
+                const data = {
+                    nome: this.name,
+                    ativo: this.ativo,
+                }
+                //requisicao post
+                const dataJson = JSON.stringify(data)
+                const req = await fetch("http://localhost:3000/product", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: dataJson
+                });
+                const res = await req.json()
+                console.log(res)
+                this.msg = "Cadastro realizado com sucesso!"
+                // // limpar campos
+                this.name = ""
+                this.ativo = null
+                setInterval(()=>this.msg="" ,3000)
+
+            } else { 
+                //validar campo vazio
+                e.preventDefault()
+                this.error = 'O campos nome não pode ficar em Vazio!!!'
+                setInterval(()=>this.error="" ,5000)
+                
+            }
+        }
+    }
 
 }
 </script>
 
-<style  scoped>
+<style lang="sass" scoped>
+.container-form 
+    max-width: 100%
+    margin: 0 auto
+    padding: 25px 0
+    background: #F5F5DC
+    
+.container-input
+    width:80%
+    display: flex
+    flex-direction: column
+    margin-bottom: 20px
+.container-input label, .submit-btn
+    text-align: left
+    margin-left: 19%
+    
+label
+    font-weight: bold
+    margin-bottom: 15px
+    color: #222
+    padding: 5px 10px
+    border-left: 4px solid #fcba03
+    font-size: 22px
 
+input,select
+    padding: 5px 10px
+    width: 60%
+.radio
+    display: flex
+    width: 60%
+    margin-bottom: 10px
+    justify-content: flex-start
+    flex-direction: row
 
-    #burger-form {
-    max-width: 400px;
-    margin: 0 auto;
-  }
-  .input-container {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-  }
-  label {
-    font-weight: bold;
-    margin-bottom: 15px;
-    color: #222;;
-    padding: 5px 10px;
-    border-left: 4px solid #fcba03;
-  }
-  input, select {
-    padding: 5px 10px;
-    width: 300px;
-  }
-  #opcionais-container {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-  #opcionais-title {
-    width: 100%;
-  }
-  .checkbox-container {
-    display: flex;
-    align-items: flex-start;
-    width: 50%;
-    margin-bottom: 20px;
-  }
-  .checkbox-container span,
-  .checkbox-container input {
-    width: auto;
-  }
-  .checkbox-container span {
-    margin-left: 6px;
-    font-weight: bold;
-  }
-  .submit-btn {
-    background-color: #222;
-    color:#fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-  }
-  .submit-btn:hover {
-    background-color: transparent;
-    color: #222;
-  }
+#flag
+    width: 10%
+    margin: 0
+    padding: 0
+    
+.radio label
+    border-left: none
+    margin: 0
+    padding: 0
+.submit-btn
+    text-align: center
+    background-color: #222
+    color:#fcba03
+    font-weight: bold
+    border: 2px solid #222
+    padding: 10px
+    font-size: 16px
+    margin: 0 auto
+    cursor: pointer
+    transition: .5s    
+.submit-btn:hover 
+    background-color: transparent
+    color: #222
+.messege
+    background: green
+    width:  60%
+    text-align: center
+    color: #fff
+    font-weight: bold
+    font-size: 18px
+.error
+    background: red
+    width:  60%
+    text-align: center
+    color: #fff
+    font-weight: bold
+    font-size: 18px
 </style>

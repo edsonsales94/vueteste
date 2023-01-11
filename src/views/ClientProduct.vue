32435720101d982d1790984a507ef3dc84696370
Edson Sales <edson.sales94@outlook.com>
2023-01-10 21:59:39 -0400

    commit: desenvovimento das tabelas, setup consuntando a APi e populando nas tabelas, salvando a associação do cliente e dos produtos no json-sever

--- src/views/ClientProduct.vue
+++ src/views/ClientProduct.vue
@@ -1,43 +1,64 @@
 <template>
-    <div class="container">
-        <table class="content-table">
-            <thead>
-                <tr>
-                    <th>Codigo</th>
-                    <th>Nome</th>
-                    <th>Documento</th>
-                    <th>Telefone</th>
-                    <th>email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in clientes" :key="index">
-                    <td>{{ item.id }}</td>
-                    <td> {{ item.name }}</td>
-                    <td>{{ item.doc }}</td>
-                    <td>{{ item.phone }}</td>
-                    <td>{{ item.email }}</td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="container-main">
+        <div class="container">
+            <table class="content-table ">
+                <thead>
+                    <tr>
+                        <th for="radio">Selecine</th>
+                        <th>Codigo</th>
+                        <th>Nome</th>
+                        <th>Documento</th>
+                        <th>Telefone</th>
+                        <th>email</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody class="scroller">
+                    <tr v-for="(item, index) in clientes" :key="index">
+                        <td>
+                            <input v-if="item.ativo == true" type="radio" name="radio" v-model="clienteObj"
+                                :value="item">
+                            <input v-else disabled type="radio" name="radio" v-model="clienteObj" :value="item">
+                        </td>
+                        <td>{{ item.id }}</td>
+                        <td> {{ item.name }}</td>
+                        <td>{{ item.doc }}</td>
+                        <td>{{ item.phone }}</td>
+                        <td>{{ item.email }}</td>
+                        <td v-if="item.ativo == true">Ativo</td>
+                        <td v-else>Inativo</td>
+                    </tr>
+                </tbody>
+            </table>
 
-        <table class="content-table">
-            <thead>
-                <tr>
-                    <th>Codigo</th>
-                    <th>Descrição</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item,index) in produtos" :key="index">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.nome }}</td>
-                    <td v-if="item.ativo == true">Ativo</td>
-                    <td v-else>Inativo</td>
-                </tr>
-            </tbody>
-        </table>
+
+                <table class="content-table">
+                    <thead>
+                        <tr>
+                            <th>Selecine</th>
+                            <th>Codigo</th>
+                            <th>Descrição</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, index) in produtos" :key="index">
+                            <td>
+                                <input v-if="item.ativo == true" type="checkbox" name="radio" v-model="arrayproduct"
+                                    :value="item">
+                                <input v-else disabled type="checkbox" name="radio" v-model="arrayproduct" :value="item">
+                            </td>
+                            <td>{{ item.id }}</td>
+                            <td>{{ item.nome }}</td>
+                            <td v-if="item.ativo == true">Ativo</td>
+                            <td v-else>Inativo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <button @click.prevent="save">Salvar</button>
+        <!-- {{ clienteObj }}
+    {{ arrayproduct }} -->
     </div>
 </template>
 
@@ -46,12 +67,14 @@ export default {
     name: "clientproduct",
     data() {
         return {
-            name: null,
+            namecli: null,
             document: null,
             phone: null,
             email: null,
             clientes: [],
-            produtos: []
+            produtos: [],
+            clienteObj: null,
+            arrayproduct: []
         }
     },
     methods: {
@@ -66,6 +89,21 @@ export default {
             const data = await req.json()
             this.produtos = data
             console.log(this.produtos, 'PRODUTOS')
+        },
+        async save() {
+            const data = {
+                clienteObj: this.clienteObj,
+                arrayproduct: this.arrayproduct
+
+            }
+            // return console.log(data,'data')
+            //     //requisicao post
+            const dataJson = JSON.stringify(data)
+            const req = await fetch("http://localhost:3000/associar", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
         }
     },
     mounted() {
@@ -86,7 +124,6 @@ export default {
     font-size: 0.9em
     width: 40%
     border-radius: 5px 5px 0 0
-    overflow: hidden
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.15)
 
 .content-table thead tr 
@@ -116,6 +153,4 @@ export default {
 .content-table tbody tr.active-row 
     font-weight: bold
     color: #009879
-
-
 </style>
